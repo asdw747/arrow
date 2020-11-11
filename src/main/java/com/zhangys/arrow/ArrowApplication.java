17cb8475ff0e41926938b36b237c73f9ec0cc7cf
zhangyingsong <zhangyingsong@xiaomi.com>
2020-11-11 12:38:55 +0800

    arrow init

---
+++ src/main/java/com/zhangys/arrow/ArrowApplication.java
@@ -0,0 +1,13 @@
+package com.zhangys.arrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArrowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhangys/arrow/ArrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangys.arrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
